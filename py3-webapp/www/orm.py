2a80f3e2d05622efca1cacb3b456a91754d38f41
pardon110Homestead <723801243@qq.com>
2017-10-20 08:33:12 +0000

    fix async with

--- py3-webapp/www/orm.py
+++ py3-webapp/www/orm.py
@@ -19,7 +19,7 @@ def log(sql,args=()):
 async def create_pool(loop,**kw):
 	logging.info('create database connection pool...')
 	global __pool
-	__pool = await aiomysql.create_pool(
+	__pool = yield from  aiomysql.create_pool(
 			host=kw.get('host','localhost'),
 			port=kw.get('port',3306),
 			user=kw.get('user','root'),
