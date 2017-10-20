a829e6f5bd6b2a83c4182180b651205a8d072bbc
pardon110Homestead <723801243@qq.com>
2017-10-20 11:12:17 +0000

    add config file

---
+++ py3-webapp/www/config_default.py
@@ -0,0 +1,21 @@
+# !python3
+# coding:utf-8
+'''
+Default configuragtions.
+'''
+
+__author__ = 'Pardon110'
+
+configs = {
+	'debug':True,
+	'db':{
+		'host':'127.0.0.1',
+		'port':3306,
+		'user':'root',
+		'password':'secret',
+		'db':'py3web'
+	},
+	'session':{
+		'secret':'Pardon110'
+	}
+}
