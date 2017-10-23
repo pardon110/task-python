1627b8dca87cac054ffd3657e45c12339172d4a5
pardon110Homestead <723801243@qq.com>
2017-10-23 13:12:34 +0000

    构建前端

--- py3-webapp/www/handlers.py
+++ py3-webapp/www/handlers.py
@@ -13,8 +13,19 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-	users = await User.findAll()
+#	users = await User.findAll()
+#	return {
+#		'__template__':'test.html',
+#		'users':users
+#	}
+
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id='1',name='Test Blog',summmary=summary,created_at=time()-120),
+		Blog(id='2',name='Something New',summmary=summary,created_at=time()-3600),
+		Blog(id='3',name='Learn Swift',summmary=summary,created_at=time()-7200),
+	]
 	return {
-		'__template__':'test.html',
-		'users':users
+		'__template__':'blogs.html',
+		'blogs':blogs
 	}
